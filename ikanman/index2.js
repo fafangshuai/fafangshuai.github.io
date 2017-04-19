8da353e600f24c06c120eb12537495d977778ef1
ffs <ghost_in@the.shell>
2017-04-19 18:11:47 +0800

    change comic

--- ikanman/index2.js
+++ ikanman/index2.js
@@ -1,13 +1,175 @@
-(function () {
+var DataService = (function () {
     var IMAGE_HOST = "http://p.yogajx.com";
+    var configPath = "config.json";
+
+    function getImagePath(path, file) {
+        var pathPrefix = IMAGE_HOST + path;
+        return pathPrefix + handleFileName(file);
+    }
+
+    function handleFileName(fileName) {
+        var idx = fileName.lastIndexOf(".webp");
+        if (idx > 0) {
+            fileName = fileName.substring(0, fileName.lastIndexOf(".webp"));
+        }
+        return fileName;
+    }
+
+    return {
+        getBooks: function () {
+            var books = [];
+            $.ajax({
+                url: configPath,
+                type: "get",
+                dataType: "json",
+                success: function (data) {
+                    $.each(data, function () {
+                        books.push(new Book(this.id, this.name, this.path));
+                    });
+                },
+                async: false
+            });
+            return books;
+        },
+        getChapters: function (path) {
+            if (!path) {
+                return null;
+            }
+            var chapters = [];
+            $.ajax({
+                url: path,
+                type: "get",
+                dataType: "text",
+                success: function (data) {
+                    var chapterJsonArr = data.split("\n");
+                    $.each(chapterJsonArr, function () {
+                        var chapter = JSON.parse(this);
+                        var files = [];
+                        $.each(chapter.files, function () {
+                            files.push(getImagePath(chapter.path, this));
+                        });
+                        chapters.push(new Chapter(chapter.cid, chapter.bname + " " + chapter.cname, files));
+                    });
+                },
+                async: false
+            });
+            return chapters;
+        }
+    }
+})();
+
+function Book(id, name, path) {
+    var self = this;
+    this.id = id;
+    this.name = name;
+    this.path = path;
+    var chapters = null;
+    this.getChapters = function () {
+        if (chapters == null) {
+            chapters = DataService.getChapters(self.path);
+        }
+        return chapters;
+    };
+}
+function Chapter(id, name, files) {
+    this.id = id;
+    this.name = name;
+    this.files = files;
+    this.totalPage = this.files.length;
+    this.getPage = function (page) {
+        if (page < 0) {
+            page = 0;
+        }
+        if (page > this.totalPage - 1) {
+            page = this.totalPage - 1;
+        }
+        return this.files[page];
+    }
+}
+var ComicReader = (function () {
+    var $viewer = $("#viewer");
+    var $pageSelect = $("#pageNav").find("select");
+    var $catalogSelect = $("#catalogNav").find("select");
+    var $bookSelect = $("#bookNav").find("select");
+
+    var model = {
+        bookMap: {},
+        chapterMap: {},
+        chapterIds: []
+    };
+
+    var view = {
+        generatePageSelect: function (totalPage) {
+            var opts = "";
+            for (var i = 0; i < totalPage; i++) {
+                var text = "第" + (i + 1) + "页";
+                var selected = i == 0 ? ' selected="selected"' : '';
+                opts += '<option value="' + i + '"' + selected + '>' + text + '</option>';
+            }
+            $pageSelect.html($(opts));
+        },
+        generateCatalogSelect: function (chapters) {
+            var opts = "";
+            for (var i = 0, len = chapters.length; i < len; i++) {
+                var chapter = chapters[i];
+                var selected = i == 0 ? ' selected="selected"' : '';
+                opts += '<option value="' + chapter.id + '"' + selected + '>' + chapter.name + '</option>';
+            }
+            $catalogSelect.html($(opts));
+        },
+        generateBookSelect: function (books) {
+            var opts = "<option selected='selected'>请选择</option>";
+            for (var i = 0, len = books.length; i < len; i++) {
+                var book = books[i];
+                opts += '<option value="' + book.id + '">' + book.name + '</option>';
+            }
+            $bookSelect.html($(opts));
+        },
+        generateViewer: function (src, nextSrc) {
+            $viewer.find("#lightImg").attr("src", src);
+            $viewer.find("#shadowImg").attr("src", nextSrc);
+        }
+    };
+
+    var ctrl = {
+        page: 0,
+        chapter: null,
+        book: null,
+        usePreLoad: false,
+        gotoPage: function (page) {
+            this.page = page * 1;
+
+        },
+        changeChapter: function (cid) {
+            $catalogSelect.val(cid);
+            this.chapter = model.chapterMap[cid];
+            view.generatePageSelect()
+        },
+        changeBook: function (bid) {
+            $bookSelect.val(bid);
+            this.book = model.bookMap[bid];
+            view.generateCatalogSelect(this.book.getChapters());
+        },
+        loadBooks: function () {
+            var books = DataService.getBooks();
+            $.each(books, function () {
+                model.bookMap[this.id] = this;
+            });
+            view.generateBookSelect(books);
+        }
+    };
+})();
+
+;(function () {
+
     var Book = {
         chapters: [],
         chapterIds: [],
         chapterMap: {}
     };
     var $viewer = $("#viewer");
-    var $pageSelect = $("#pageSelect").find("select");
-    var $catalogSelect = $("#catalog").find("select");
+    var $pageSelect = $("#pageNav").find("select");
+    var $catalogSelect = $("#catalogNav").find("select");
     var usePreLoad = false;
 
     function loadData() {
@@ -70,41 +232,25 @@
         return fileName;
     }
 
-    function generateCatalog() {
-        var select = $('<select class="form-control"></select>');
-        var opts = "";
-        for (var i = 0, len = Book.chapters.length; i < len; i++) {
-            var chapter = Book.chapters[i];
-            var chapterName = chapter.bname + "" + chapter.cname;
-            var selected = i == 0 ? ' selected="selected"' : '';
-            opts += '<option value="' + chapter.cid + '"' + selected + '>' + chapterName + '</option>';
-        }
-        return $(opts);
+    function generateBookSelect() {
+
     }
 
-    function generatePageSelect(totalPage) {
-        var opts = "";
-        for (var i = 0; i < totalPage; i++) {
-            var text = "第" + (i + 1) + "页";
-            var selected = i == 0 ? ' selected="selected"' : '';
-            opts += '<option value="' + i + '"' + selected + '>' + text + '</option>';
-        }
-        return $(opts);
-    }
 
     var CookieUtil = {
         options: {expires: 365, path: "/"},
-        setCid: function (cid) {
-            Cookies.set("cid", cid, this.options);
+        setCidAndPage: function (cid, page) {
+            var bid = this.getBid();
+            Cookies.set("ck" + bid, {"cid": cid, "page": page}, this.options);
         },
-        setPage: function (page) {
-            Cookies.set("page", page, this.options);
+        getCidAndPage: function () {
+            return Cookies.getJSON("ck" + this.getBid());
         },
-        getCid: function () {
-            return Cookies.get("cid");
+        setBid: function (bid) {
+            Cookies.set("bid", bid, this.options);
         },
-        getPage: function () {
-            return Cookies.get("page");
+        getBid: function () {
+            return Cookies.get("bid");
         },
         remove: function (key) {
             Cookies.reomve(key);
@@ -112,7 +258,7 @@
     };
 
     function initCatalog() {
-        $catalogSelect.html(generateCatalog());
+        $catalogSelect.html(generateCatalogSelect());
     }
 
     function bindEvent() {
